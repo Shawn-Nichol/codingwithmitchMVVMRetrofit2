23dfb72096576744cb78ece6fef423ec184061fb
Shawn <7067shawnnichol@gmail.com>
2020-02-29 18:00:44 -0700

    03) Testing Retrofit Response

---
+++ app/src/main/java/com/example/codingwithmitchmvvmretrofit2/models/Post.java
@@ -0,0 +1,29 @@
+package com.example.codingwithmitchmvvmretrofit2.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+
+    private int userId;
+
+    private int id;
+
+
+
+    @SerializedName("body")
+    private String text;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public String getText() {
+        return text;
+    }
+}

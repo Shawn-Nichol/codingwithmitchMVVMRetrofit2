b11c7a2cc2ed1f03f36b95a3c88dc08a4bbbf49a
Shawn <7067shawnnichol@gmail.com>
2020-03-22 13:24:54 -0600

    18)NetworkTimeout

--- app/src/main/java/com/example/codingwithmitchmvvmretrofit2/viewmodel/RecipeViewModel.java
+++ app/src/main/java/com/example/codingwithmitchmvvmretrofit2/viewmodel/RecipeViewModel.java
@@ -10,13 +10,19 @@ public class RecipeViewModel extends ViewModel {
 
     private RecipeRepository mRecipeRepository;
     private String mRecipeId;
+    private boolean mDidRetrievedRecipe;
 
     public RecipeViewModel() {
         mRecipeRepository = RecipeRepository.getInstance();
+        mDidRetrievedRecipe = false;
     }
 
     public LiveData<Recipe> getRecipe() {
         return mRecipeRepository.getRecipe();
+    }
+
+    public LiveData<Boolean> isRecipeRequestTimedOut() {
+        return mRecipeRepository.isRecipeRequestTimedOut();
     }
 
     public void searchRecipeById(String recipeId) {
@@ -26,5 +32,13 @@ public class RecipeViewModel extends ViewModel {
 
     public String getRecipeId() {
         return mRecipeId;
+    }
+
+    public void setRetrievedRecipe(boolean retrievedRecipe) {
+        mDidRetrievedRecipe = mDidRetrievedRecipe;
+    }
+
+    public boolean didRetrieveRecipe() {
+        return mDidRetrievedRecipe;
     }
 }
